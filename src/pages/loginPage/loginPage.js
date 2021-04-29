40403b15c6964074b0b55ad70bf1f775ac994a57
Manuel Humberto Rodríguez Villegas <mhrv99@gmail.com>
2021-04-29 00:07:58 -0500

    Add blur login

---
+++ src/pages/loginPage/loginPage.js
@@ -0,0 +1,10 @@
+import './loginPage.css';
+import Buttons from '../../components/login/buttons';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Buttons />
+    </div>
+  );
+}
